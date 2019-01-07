f69d72351c9cee78e962248aec49c69485f3c204
Lukáš Mráček <luky@lukas-macbook-pro.local>
2019-01-07 15:44:30 +0100

    Convert to MVC design pattern.

---
+++ Todoey/New Group/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Lukáš Mráček on 07/01/2019.
+//  Copyright © 2019 Lukáš Mráček. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
